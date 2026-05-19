8645499f20b0ddcaee6bb5403959545f48f7cde4
z1lV3r <zilver.thd@gmail.com>
2026-05-19 17:21:44 -0600

    #2 Save custom name in local storage

--- code/src/features/tab-rebrand/presentation/background/message-events/set-custom-name-use-cases-listeners/set-current-tab-custom-name-message-event-listener.ts
+++ code/src/features/tab-rebrand/presentation/background/message-events/set-custom-name-use-cases-listeners/set-current-tab-custom-name-message-event-listener.ts
@@ -1,19 +1,19 @@
+import type { SetTabCustomNameUseCases } from "@/features/tab-rebrand/use-cases/set-tab-custom-name-use-cases";
 import type { MessageEventListener } from "@/shared/domain/models/message-event-listener";
 
 export class SetCurrentTabCustomNameMessageEventListener implements MessageEventListener {
-  //private readonly setTabCustomNameUseCases: SetTabCustomNameUseCases;
+  private readonly setTabCustomNameUseCases: SetTabCustomNameUseCases;
 
-  constructor(/*setTabCustomNameUseCases: SetTabCustomNameUseCases*/) {
-//    this.setTabCustomNameUseCases = setTabCustomNameUseCases;
+  constructor(setTabCustomNameUseCases: SetTabCustomNameUseCases) {
+    this.setTabCustomNameUseCases = setTabCustomNameUseCases;
   }
 
   name = "set-current-tab-custom-name-message-event-listener";
   description = "Set current tab custom name message event listener";
 
   async command(_request: any, _sender: any, sendResponse: (response: any) => void): Promise<void> {
-    //const url = await this.setCheckpointUseCases.setCurrentTabCheckpoint();
-    //sendResponse({ success: true, data: url });
-    console.info("Set current tab custom name message event listener");
-    sendResponse({ success: true });
+    const name = _request.name;
+    const tabCustomName = await this.setTabCustomNameUseCases.setCurrentTabCustomName(name);
+    sendResponse({ success: true, data: tabCustomName });
   }
 }
