98e81e93a7f0b4460ca8a3a3e07b939890f13b0c
z1lV3r <zilver.thd@gmail.com>
2026-06-02 13:39:30 -0600

    Fixing things

--- code/extensions/tab-checkpoint/entrypoints/background.ts
+++ code/extensions/tab-checkpoint/entrypoints/background.ts
@@ -2,6 +2,6 @@ import { createI18n } from "@wxt-dev/i18n";
 
 export const i18n = createI18n();
 
-export default defineBackground(async () => {
+export default defineBackground(() => {
   console.log(i18n.t("helloWorld"));
 });
