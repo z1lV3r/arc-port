49036fd129f8c25f25b484c81dec11923cabfaee
z1lV3r <zilver.thd@gmail.com>
2026-06-02 13:39:30 -0600

    Fixing things

--- code/extensions/tab-checkpoint/entrypoints/popup/App.tsx
+++ code/extensions/tab-checkpoint/entrypoints/popup/App.tsx
@@ -1,8 +1,10 @@
 import Demo from "@repo/shared/presentation/demo";
 import "./App.css";
+import { createI18n } from "@wxt-dev/i18n";
 
 function App() {
-  return <Demo />;
+  const i18n = createI18n();
+  return <Demo t={i18n.t("helloWorld")} />;
 }
 
 export default App;
